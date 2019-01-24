fab8fca14bf83364b8ba2aa521d1be6a26fa2110
spathak1 <spathak1@GOT500VI73157.vccnet.volvocars.net>
2019-01-25 00:37:29 +0530

    Test1 changed

--- src/com/traning/git/Test1.java
+++ src/com/traning/git/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Inside test1");
+        System.out.println("Inside test1 git");
 
     }
 
